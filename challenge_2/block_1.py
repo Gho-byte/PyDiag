2e2373ad1c7e1d49b583634cce364b9dd15e6fb5
Gho-byte <mohamedayllafkih@gmail.com>
2026-09-03 11:03:32 +0100

    finishing the first block

---
+++ challenge_2/block_1.py
@@ -0,0 +1,59 @@
+def get_min_max(notes):
+    min = notes[0] ; max = notes[0]
+    for note in notes:
+        if note > max:
+            max = note
+        elif note < min:
+            min = note
+    print(f'[+] Note Max: {max}')
+    print(f'[+] Note Min: {min}')
+
+def notes_au_dessus(notes, seuil):
+    output = []
+    for note in notes:
+        if note >= seuil:
+            output.append(note)
+    print(output)
+
+def comptage(fruits):
+    output = {}
+    for fruit in fruits:
+        try:
+            output[fruit] = output[fruit] + 1
+        except KeyError:
+            output[fruit] = 1
+    if len(output) > 0:
+        for element in output:
+            print(element)
+    else:
+        print('[-] Please provide a list')
+
+def reverse_list(input_list):
+    output_list = []
+    if len(input_list) > 0:
+        for i in range(len(input_list) - 1, 0):
+            output_list.append(input_list[i])
+        print(output_list)
+    else:
+        print('[-] Please provide a list')
+
+def fusion_lists(list_one, list_two):
+    output = {}
+    min = len(list_one)
+    list_two_len = len(list_two)
+    if list_two_len < min: min = list_two_len
+    for i in range(min):
+        if list_one[i] < list_two[i]:
+            output.add(list_one[i]) ; output.add(list_two[i])
+        elif list_one[i] > list_two[i]:
+            output.add(list_two[i]) ; output.add(list_one[i])
+        else:
+            output.add(list_two[i])
+    print(list(output))
+
+def comprehension(notes):
+    output_list = []
+    for note in notes:
+        if note % 2 == 0:
+            output_list.append(note*note)
+    print(output_list)
