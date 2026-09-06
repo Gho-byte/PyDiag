2a7f32d338547c6ec51a2503801dc7313c15db5e
Gho-byte <mohamedayllafkih@gmail.com>
2026-09-06 15:43:24 +0100

    feat: complete the first block in the forth challenge

---
+++ challenge_4/block_1.py
@@ -0,0 +1,13 @@
+def salut(nom):
+    print(f'Salut {nom}!')
+
+def puissance(number, exp=2):
+    return number^exp
+
+def afficher_message(message):
+    print(message)
+
+def diviser_avec_reste(a, b):
+    return a // b, a % b
+
+# print(diviser_avec_reste(17, 5))
